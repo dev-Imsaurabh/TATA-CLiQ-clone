65006ec75f3894775aa9bed86e7189614904a52e
bhavnesh1811 <erbhavnesharora@gmail.com>
2023-01-18 22:22:20 +0530

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,7 @@
 import { Box } from "@chakra-ui/react";
 
-export default function Navbar(){
-
-
-    return <Box>
-        Sample navbar with folder export
-    </Box>
-
-
+export default function Navbar() {
+  return <Box>{/* Sample navbar with folder export */}
+  {/* todo navbar */}
+  </Box>;
 }
