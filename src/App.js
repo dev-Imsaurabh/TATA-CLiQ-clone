1671c7d06379f8f74f46f11b5547fb64324d06b1
bhavnesh1811 <erbhavnesharora@gmail.com>
2023-01-20 13:27:25 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AllRoutes from "./routes/AllRoutes";
 import { Navbar } from "./components/Navbar";
 import { THIS_IS_SIMPLE_CONSTANTS } from "./constants/constants";
 import Footer from "./components/Footer/Footer";
+// import Carousel from "./components/Carousel/Carousel";
 
 function App() {
   //cheking for .env file working or not
@@ -17,6 +18,7 @@ function App() {
   return (
     <Box className="App">
       <Navbar />
+      {/* <Carousel/> */}
       <AllRoutes />
       <Footer/>
     </Box>
