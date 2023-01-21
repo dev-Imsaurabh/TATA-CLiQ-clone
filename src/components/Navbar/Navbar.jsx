03ec52191960da8d6973ae3418c24ba3bb52970a
Tinkle7 <tinkledash@gmail.com>
2023-01-21 14:01:08 +0530

    second  update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -24,55 +24,112 @@ export default function Navbar() {
   const [arrow1, setArrow1] = useState(false);
   const [arrow2, setArrow2] = useState(false);
   return (
-    <Box>
-      <Box display="flex">
-        <Box maxW="15%" padding={2}>
-          <Image src={logo} />
+    <Box
+      bg="#212121"
+      color="white"
+      fontSize={{ base: ".3rem", md: ".6rem", lg: ".9rem" }}
+    >
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        bg="black"
+        w="82%"
+        float="right"
+        padding={2}
+      >
+        <Box flexGrow="1" flexBasis="0" textAlign="left">
+          <Link>Tata CLiQ Luxury</Link>
         </Box>
-        <Box>
-          <Box display="flex" justifyContent="space-between">
-            <Box>
-              <Link>Tata CLiQ Luxury</Link>
-            </Box>
-            <Box>
-              <Link>CLiQ Cash</Link>
-              <Link>Gift Card</Link>
-              <Link>CLiQ Care</Link>
-              <Link>Track Orders</Link>
-              <Link>Sign in / Sign Up</Link>
+        <Box
+          display="flex"
+          justifyContent="space-evenly"
+          flexGrow="1"
+          flexBasis="0"
+        >
+          <Link>CLiQ Cash</Link>
+          <Link>Gift Card</Link>
+          <Link>CLiQ Care</Link>
+          <Link>Track Orders</Link>
+          <Link>Sign in / Sign Up</Link>
+        </Box>
+      </Box>
+      <Box
+        display="flex"
+        w="100%"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Box
+          padding={2}
+          flexGrow="1"
+          flexBasis="0"
+          position="relative"
+          bottom={5}
+        >
+          <Link>
+            <Image w="100%" src={logo} />
+          </Link>
+        </Box>
+        <Box
+          flexGrow="7"
+          flexBasis="0"
+          display="flex"
+          alignItems="center"
+          justifyContent="space-evenly"
+        >
+          <Box
+            flexGrow="2"
+            flexBasis="0"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              w="75%"
+              margin="auto"
+            >
+              <Box
+                display="flex"
+                onMouseEnter={() => setArrow1(true)}
+                onMouseLeave={() => setArrow1(false)}
+              >
+                <Text>Categories</Text>
+                <Box>{arrow1 ? <ChevronUpIcon /> : <ChevronDownIcon />}</Box>
+              </Box>
+              <Box
+                display="flex"
+                onMouseEnter={() => setArrow2(true)}
+                onMouseLeave={() => setArrow2(false)}
+              >
+                <Text>Brands</Text>
+                <Box>{arrow2 ? <ChevronUpIcon /> : <ChevronDownIcon />}</Box>
+              </Box>
             </Box>
           </Box>
-          <Box display="flex" justifyContent="center" alignItems="center">
-            <Box
-              display="flex"
-              onMouseEnter={() => setArrow1(true)}
-              onMouseLeave={() => setArrow1(false)}
-            >
-              <Text>Categories</Text>
-              <Box>{arrow1 ? <ChevronUpIcon /> : <ChevronDownIcon />}</Box>
+          <Box flexGrow="4" flexBasis="0">
+            <InputGroup>
+              <InputLeftElement
+                pointerEvents="none"
+                children={<SearchIcon color="gray.300" />}
+              />
+              <Input type="tel" placeholder="Search for Products" />
+            </InputGroup>
+          </Box>
+          <Box
+            flexGrow="2"
+            flexBasis="0"
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Box display="flex" justifyContent="center" alignItems="center">
+              <BsSuitHeart size="18%" />
             </Box>
-            <Box
-              display="flex"
-              onMouseEnter={() => setArrow2(true)}
-              onMouseLeave={() => setArrow2(false)}
-            >
-              <Text>Brands</Text>
-              <Box>{arrow2 ? <ChevronUpIcon /> : <ChevronDownIcon />}</Box>
-            </Box>
-            <Box>
-              <InputGroup>
-                <InputLeftElement
-                  pointerEvents="none"
-                  children={<SearchIcon color="gray.300" />}
-                />
-                <Input type="tel" placeholder="Phone number" />
-              </InputGroup>
-            </Box>
-            <Box>
-              <BsSuitHeart />
-            </Box>
-            <Box>
-              <FiShoppingBag />
+            <Box display="flex" justifyContent="center" alignItems="center">
+              <FiShoppingBag size="18%" />
             </Box>
           </Box>
         </Box>
